a82b14242f80dbc9b68b2861c90df5180f4b6d5b
Isaac <izaak343@gmail.com>
2018-10-04 23:56:08 -0500

    added home.php

---
+++ home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="src/css/carbon.css">
+        <!-- <link rel="stylesheet" href="/src/css/main.css"> -->
+    </head>
+    <body>
+        <?php
+            include 'halo-planets.php';
+        ?>
+        <div class="bx--grid">
+            <div class="bx--row">
+                <div class="bx--col-xs-12">
+                    <center><h1>Turista Multiversal</h1></center>
+                </div>
+            </div>
+            <div class="bx--row">
+                <div class="bx--col-xs-1 ">
+                    Planeta
+                </div>
+                <div class="bx--col-xs-1">
+                    Costo
+                </div>
+                <div class="bx--col-xs-1">
+                    Renta base
+                </div>
+                <div class="bx--col-xs-1">
+                    Tier 1
+                </div>
+                <div class="bx--col-xs-1">
+                    Tier 2
+                </div>
+                <div class="bx--col-xs-1">
+                    Tier 3
+                </div>
+                <div class="bx--col-xs-1">
+                    Tier 4
+                </div>
+                <div class="bx--col-xs-1">
+                    Tier 5
+                </div>
+                <div class="bx--col-xs-1">
+                    Costo de Mejora
+                </div>
+                <div class="bx--col-xs-1">
+                    Hipoteca
+                </div>
+                <div class="bx--col-xs-1">
+                    Universo
+                </div>
+                <div class="bx--col-xs-1">
+                    Bonus
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
